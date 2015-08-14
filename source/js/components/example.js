f49157fe56b03b4f1759b79b1ed2e5cc4581a54e
Julian <renard.julian@gmail.com>
2015-08-14 10:10:22 +0200

    Use skeleton lib like css boilerplate

---
+++ source/js/components/example.js
@@ -0,0 +1,28 @@
+// /**
+//  * An example file of the JS structure
+//  * -----------------------------------------------------------------------------
+//  */
+'use strict';
+
+var $ = require('jQuery'),
+    Example;
+
+Example = function(options) {
+    this.$element = $(options.element);
+
+    this.init();
+};
+
+Example.prototype.init = function() {
+    if (this.$element.length === 0) {
+        return;
+    }
+
+    this.foo();
+};
+
+Example.prototype.foo = function() {
+    console.log('bar');
+};
+
+module.exports = Example;
