544a6ecc9c6c393c7bb80040244e8d2bf3f89937
Julian <renard.julian@gmail.com>
2015-08-14 14:45:54 +0200

    Add responsive-nav.js

--- source/js/main.js
+++ source/js/main.js
@@ -4,17 +4,22 @@
 var $ 			= require('jQuery'),
 	Modernizr   = require('modernizr');
 
+
 window.jQuery = window.$ = $;
 
 $(document).ready(function() {
     var Example             = require('./components/example'),
         MediaQueryListener  = require('./components/mediaquerylistener'),
+        ResponsiveNav = require('./vendor/responsive-nav'),
         example,
-        mediaquerylistener;
+        mediaquerylistener,
+        ResponsiveNav;
 
     $('.element').each(function(i, elem) {
         example = new Example({element: elem});
     });
 
     mediaquerylistener = new MediaQueryListener();
+
+    var nav = ResponsiveNav(".nav-collapse");
 });
